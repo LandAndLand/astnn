4163a2b2572d4b83d52ff5f419a3e6dd0bafdf79
land <1138578898@qq.com>
2020-11-11 22:05:56 +0800

    m

--- clone/model.py
+++ clone/model.py
@@ -22,7 +22,8 @@ class BatchTreeEncoder(nn.Module):
         self.max_index = vocab_size
         # pretrained  embedding
         if pretrained_weight is not None:
-            self.embedding.weight.data.copy_(torch.from_numpy(pretrained_weight))
+            self.embedding.weight.data.copy_(
+                torch.from_numpy(pretrained_weight))
             # self.embedding.weight.requires_grad = False
 
     def create_tensor(self, tensor):
@@ -34,24 +35,25 @@ class BatchTreeEncoder(nn.Module):
         size = len(node)
         if not size:
             return None
-        batch_current = self.create_tensor(Variable(torch.zeros(size, self.embedding_dim)))
+        batch_current = self.create_tensor(
+            Variable(torch.zeros(size, self.embedding_dim)))
 
         index, children_index = [], []
         current_node, children = [], []
         for i in range(size):
             # if node[i][0] is not -1:
-                index.append(i)
-                current_node.append(node[i][0])
-                temp = node[i][1:]
-                c_num = len(temp)
-                for j in range(c_num):
-                    if temp[j][0] is not -1:
-                        if len(children_index) <= j:
-                            children_index.append([i])
-                            children.append([temp[j]])
-                        else:
-                            children_index[j].append(i)
-                            children[j].append(temp[j])
+            index.append(i)
+            current_node.append(node[i][0])
+            temp = node[i][1:]
+            c_num = len(temp)
+            for j in range(c_num):
+                if temp[j][0] is not -1:
+                    if len(children_index) <= j:
+                        children_index.append([i])
+                        children.append([temp[j]])
+                    else:
+                        children_index[j].append(i)
+                        children[j].append(temp[j])
             # else:
             #     batch_index[i] = -1
 
@@ -59,19 +61,23 @@ class BatchTreeEncoder(nn.Module):
                                                           self.embedding(Variable(self.th.LongTensor(current_node)))))
 
         for c in range(len(children)):
-            zeros = self.create_tensor(Variable(torch.zeros(size, self.encode_dim)))
+            zeros = self.create_tensor(
+                Variable(torch.zeros(size, self.encode_dim)))
             batch_children_index = [batch_index[i] for i in children_index[c]]
             tree = self.traverse_mul(children[c], batch_children_index)
             if tree is not None:
-                batch_current += zeros.index_copy(0, Variable(self.th.LongTensor(children_index[c])), tree)
+                batch_current += zeros.index_copy(0, Variable(
+                    self.th.LongTensor(children_index[c])), tree)
         # batch_index = [i for i in batch_index if i is not -1]
         b_in = Variable(self.th.LongTensor(batch_index))
-        self.node_list.append(self.batch_node.index_copy(0, b_in, batch_current))
+        self.node_list.append(
+            self.batch_node.index_copy(0, b_in, batch_current))
         return batch_current
 
     def forward(self, x, bs):
         self.batch_size = bs
-        self.batch_node = self.create_tensor(Variable(torch.zeros(self.batch_size, self.encode_dim)))
+        self.batch_node = self.create_tensor(
+            Variable(torch.zeros(self.batch_size, self.encode_dim)))
         self.node_list = []
         self.traverse_mul(x, list(range(self.batch_size)))
         self.node_list = torch.stack(self.node_list)
@@ -97,23 +103,24 @@ class BatchProgramCC(nn.Module):
         self.bigru = nn.GRU(self.encode_dim, self.hidden_dim, num_layers=self.num_layers, bidirectional=True,
                             batch_first=True)
         # linear
-        self.hidden2label = nn.Linear(self.hidden_dim * 2, self.label_size)
+        self.hidden2label = nn.Linear(
+            self.hidden_dim * 2 * 30, self.label_size)
         # hidden
         self.hidden = self.init_hidden()
         self.dropout = nn.Dropout(0.2)
 
         # 关于attention的
-        self.W_s1 = nn.Linear(2* self.hidden_dim, 350)
+        self.W_s1 = nn.Linear(2 * self.hidden_dim, 350)
         self.W_s2 = nn.Linear(350, 30)
-		self.W_s2 = nn.Linear(350, 30)
-		# self.fc_layer = nn.Linear(30*2*hidden_size, 2000)
-
+        # self.fc_layer = nn.Linear(30*2*hidden_size, 2000)
 
     def init_hidden(self):
         if self.gpu is True:
             if isinstance(self.bigru, nn.LSTM):
-                h0 = Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim).cuda())
-                c0 = Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim).cuda())
+                h0 = Variable(torch.zeros(self.num_layers * 2,
+                                          self.batch_size, self.hidden_dim).cuda())
+                c0 = Variable(torch.zeros(self.num_layers * 2,
+                                          self.batch_size, self.hidden_dim).cuda())
                 return h0, c0
             return Variable(torch.zeros(self.num_layers * 2, self.batch_size, self.hidden_dim)).cuda()
         else:
@@ -124,29 +131,31 @@ class BatchProgramCC(nn.Module):
         if self.gpu:
             return zeros.cuda()
         return zeros
+
     def attention_net(self, bigru_out):
         """
         Now we will use self attention mechanism to produce a matrix embedding of the input sentence in which every row represents an
-		encoding of the input sentence but giving an attention to a specific part of the sentence. 
+                encoding of the input sentence but giving an attention to a specific part of the sentence. 
 
         We will use 30 such embedding of the input sentence and then finally we will concatenate all the 30 sentence embedding vectors and connect it to a fully 
-		connected layer of size 2000 which will be connected to the output layer of size 2 returning logits for our two classes i.e., 
-		pos & neg.
+                connected layer of size 2000 which will be connected to the output layer of size 2 returning logits for our two classes i.e., 
+                pos & neg.
 
-		Arguments
-		---------
-		lstm_output = A tensor containing hidden states corresponding to each time step of the LSTM network.
-		---------
-		Returns : Final Attention weight matrix for all the 30 different sentence embedding in which each of 30 embeddings give
-				  attention to different parts of the input sentence.
-		Tensor size : lstm_output.size() = (batch_size, num_seq, 2*hidden_size)
-					  attn_weight_matrix.size() = (batch_size, 30, num_seq)
-		"""
-		attn_weight_matrix = self.W_s2(F.tanh(self.W_s1(bigru_out)))
-		attn_weight_matrix = attn_weight_matrix.permute(0, 2, 1)
-		attn_weight_matrix = F.softmax(attn_weight_matrix, dim=2)
+                Arguments
+                ---------
+                lstm_output = A tensor containing hidden states corresponding to each time step of the LSTM network.
+                ---------
+                Returns : Final Attention weight matrix for all the 30 different sentence embedding in which each of 30 embeddings give
+                                  attention to different parts of the input sentence.
+                Tensor size : lstm_output.size() = (batch_size, num_seq, 2*hidden_size)
+                                          attn_weight_matrix.size() = (batch_size, 30, num_seq)
+                """
 
-		return attn_weight_matrix
+        attn_weight_matrix = self.W_s2(F.tanh(self.W_s1(bigru_out)))
+        attn_weight_matrix = attn_weight_matrix.permute(0, 2, 1)
+        attn_weight_matrix = F.softmax(attn_weight_matrix, dim=2)
+
+        return attn_weight_matrix
 
     def encode(self, x):
         # 取一个batch中的所有ast的 最大语句树个数
@@ -172,11 +181,11 @@ class BatchProgramCC(nn.Module):
         encodes = encodes.view(self.batch_size, max_len, -1)
         # return encodes
         gru_out, hidden = self.bigru(encodes, self.hidden)
-        # print(gru_out.size()) 
+        # print(gru_out.size())
         # (batch_size, num_seq, 2*encode_dim) 是输入到attention的表示
         # print(hidden.size())
         # (2, batch_size, encode_dim)
-        
+
         # pooling
         #gru_out = F.max_pool1d(gru_out, gru_out.size(2)).squeeze(2)
         # gru_out = gru_out[:,-1]
@@ -188,9 +197,10 @@ class BatchProgramCC(nn.Module):
         # hidden_matrix.size():[batch_size, 30, 2*embedding_dim]
 
         # fully connected
-        gru_with_attn_out = hidden_matrix.view(-1, hidden_matrix.size()[1]*hidden_matrix.size()[2])
+        gru_with_attn_out = hidden_matrix.view(
+            -1, hidden_matrix.size()[1]*hidden_matrix.size()[2])
         # hidden_matrix.size(): [batch_size, 30*2*embedding_dim]
-        
+
         return gru_with_attn_out
 
     # x1和x2都是输入的一个batch，每个batch包含：32个样本，每个样本是由ast拆分得到的所有语句树序列组成的
@@ -200,10 +210,9 @@ class BatchProgramCC(nn.Module):
 
     def forward(self, x1, x2):
         lvec, rvec = self.encode(x1), self.encode(x2)
-
+        print(lvec.size(), rvec.size())
         # 一维范数计算两个编码的距离
         abs_dist = torch.abs(torch.add(lvec, -rvec))
-
+        print(abs_dist.size())
         y = torch.sigmoid(self.hidden2label(abs_dist))
         return y
-
